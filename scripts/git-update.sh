679f32a2f2ebbfa43baf063563059b8acb1f3261
Brian Albertson <balbertson@marvell.com>
2023-04-28 11:03:46 -0500

    Used full path for calling user prompt script. Also did some comment cleanup.

--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+# This script implements a process that first determines whether a repository can be
+# fast-forwarded, then fast-forwards if possible. There are some cases where user input
+# is required to decide whether to fast-forward or maintain the repo state.
+this_script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
 git fetch origin HEAD
 
 # Check if the default branch is being used
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 default_branch=$(basename $(git rev-parse --abbrev-ref origin/HEAD))
 if [[ "$current_branch" != "$default_branch" ]]; then
-    read -p "Not in default branch. Assuming developer and aborting update."
+    echo "Current branch ($current_branch) differs from default branch ($default_branch)."
+    read -p "Assuming developer and aborting update."
     exit 0
 fi
 
@@ -16,7 +22,7 @@ if git diff --quiet --exit-code HEAD..origin/HEAD; then
     exit 0
 fi
 
-# Get the number of commits that HEAD is ahead of origin/HEAD
+# Get the number of commits that origin/HEAD is ahead of HEAD
 ahead=$(git rev-list --left-right --count origin/HEAD...HEAD | gawk '{print $2}')
 if [[ $ahead -ne 0 ]]; then
     read -p "HEAD is ahead of origin by $ahead commits. Assuming developer and aborting update."
@@ -27,7 +33,7 @@ fi
 if ! git diff --quiet --exit-code HEAD; then
     echo "You have uncommitted changes to the following files."
     git diff --name-only HEAD
-    if source user-prompt-yn.sh "Keep uncommitted change and defer update?"; then
+    if source ${this_script_dir}/user-prompt-yn.sh "Keep uncommitted change and defer update?"; then
         echo "Deferring update."
         sleep 2s
         exit 0
@@ -38,8 +44,8 @@ if ! git diff --quiet --exit-code HEAD; then
 fi
 
 echo "The following updates are available."
-git log --oneline --no-decorate HEAD~${ahead}..HEAD
-if ! source user-prompt-yn.sh "Do you accept them?"; then
+git log --oneline --no-decorate origin/HEAD..HEAD
+if ! source ${this_script_dir}/user-prompt-yn.sh "Do you accept them?"; then
     echo "Deferring update."
     sleep 2s
     exit 0
